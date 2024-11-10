f4b91ed266b1b04c290363b9e877a24678574f7a
wxamlzm <zd5024@163.com>
2024-11-10 10:59:17 +0800

    Fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 app.use(Antd)
